8eac596733268ebe2453f58c1900670b2f3c3e4f
Tanmoy2016 <ghoshal.tanmoy@gmail.com>
2022-04-29 13:49:29 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,7 +3,8 @@ public class Test
 
   public static void main (String[] args)
   {
-     System.out.println*"Hello Tanmoy");
+     System.out.println("Hello Tanmoy");
+     System.out.println("Hello Again");
    }
    
 }
